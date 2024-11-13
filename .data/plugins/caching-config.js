2c820c983dc4e435ba0452ef41560415daad31ab
Adam Malone <adam@adammalone.net>
2024-11-12 23:47:37 -0800

    changes cached queries

--- .data/plugins/caching-config.js
+++ .data/plugins/caching-config.js
@@ -6,18 +6,8 @@ export const Config = {
   otel_headers: {},
 
   queries_to_cache: [
-    `
-      query MyQuery {
-        customers(where: {state: {_in: ["WA", "ACT"]}}) {
-          firstName
-          lastName
-          postcode
-          state
-          country
-          city
-          address
-        }
-      }
-    `
+    `{customers(where: {state: {_in: ["WA", "ACT"]}}) {firstName lastName postcode state country city address}}`
+    `{ usersById(id: 1) { email formatCreatedAtTimestamp } customers(limit: 1) { firstName lastName email segment customerLinks { customerPreferences { socialMedia { linkedin } } supportDB { supportHistory { date status } } } creditCards { maskCreditCard expiry cvv } billings { formatBillingDate paymentStatus totalAmount } } calls(limit: 1) { callid } cdr(limit: 1) { guid } documents(limit: 1) { uuid } }`,
+    `{ customers(limit: 1) { customerId } }`
   ],
 };
